8928bbf1c00c057a97660bc6d7c60bb54154a31e
Thomas Kortyka <t.kortyka@gmx.de>
2023-10-31 00:42:09 +0100

    chore: add shared structure, add common types

---
+++ libs/shared/src/lib/index.ts
@@ -0,0 +1,7 @@
+export * from './constants';
+export * from './decorators';
+export * from './enums';
+export * from './exceptions';
+export * from './helpers';
+export * from './interfaces';
+export * from './types';
---
+++ libs/shared/src/lib/types/either.type.ts
@@ -0,0 +1,1 @@
+export type Either<TLeft, TRight> = TLeft | TRight;
