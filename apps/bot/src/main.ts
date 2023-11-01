8289a79f3ef0afc6eb433ec10975d5dbb9e715bc
Thomas Kortyka <t.kortyka@gmx.de>
2023-11-01 11:35:27 +0100

    chore: Add Example Bot inside Main

--- apps/bot/src/main.ts
+++ apps/bot/src/main.ts
@@ -1,1 +1,32 @@
-console.log('Hello World');
+import { Bot } from './bot';
+import { environment } from './environment';
+import { Events } from '@toxictoast/shared';
+
+const bot = new Bot({
+  logging: true,
+  authentication: {
+    user_id: environment.user_id,
+    client_id: environment.client_id,
+    client_secret: environment.client_secret,
+    access_token: environment.access_token,
+    refresh_token: environment.refresh_token,
+  },
+  channels: environment.channels,
+});
+
+bot.addPlugin({
+  name: 'JoinLogger',
+  event: Events.JOIN,
+  execute: (data) => {
+    console.log(data);
+  },
+});
+bot.addPlugin({
+  name: 'PartLogger',
+  event: Events.PART,
+  execute: (data) => {
+    console.log(data);
+  },
+});
+
+bot.init();
